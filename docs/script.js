f7571777b9fb9c5c4679885ab1f4e7cb06538065
kittytat <157163321+kittytat@users.noreply.github.com>
2024-02-15 22:30:13 +0000

    LAB 2

--- docs/script.js
+++ docs/script.js
@@ -7,3 +7,21 @@ window.onload = function() {
         document.getElementById('datetime').innerHTML = displayDate + " " + displayTime;
     }, 1000); // 1000 milliseconds = 1 second
 }
+
+
+/* My new function to check if the name field contain only alphabets */
+function checkName(submitcheck) {
+      var letters = /^[A-Za-z]+$/;
+      if(submitcheck.value.match(letters))
+      {
+      alert('Your name is Valid');
+      console.log('Your name is Valid');
+      return true;
+      }
+      else
+      {
+      alert('Please input alphabet characters only');
+      console.log("Please input alphabet characters only")
+      return false;
+      }
+    }
